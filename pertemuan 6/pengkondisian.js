9488d36bd3315e84a7c83b199140bd8a7aa52c31
AsrofurRizqi <asrofurrizqi@gmail.com>
2021-01-25 17:19:09 +0700

    6 - add pengkondisianjs

---
+++ pertemuan 6/pengkondisian.js
@@ -0,0 +1,57 @@
+//let plugin = 13;
+//if(plugin % 2 === 0){
+//    console.log(plugin +"adalah bilangan genap");
+//}else{
+//    console.log(plugin+"adalah bilangan ganjil");
+//}
+//for(i=0; i < 10;i++){
+//    console.log("Hello World")
+//}
+//let plugin = prompt("masukkan angka : ")
+//if(plugin%2 === 0){
+//    alert(plugin+"adalah bilangan genap");
+//}else
+//    alert(plugin+"adalah bilangan ganjil");
+//while(true){
+//    console.log("Hellow World");
+//}
+// let ulang = true
+// while(ulang){
+//     console.log("Hellow World")
+//     ulang = confirm("lagi ?")
+// }
+// let ulang = 1;
+// while(ulang <= 10){
+//     console.log("Hello World"+ulang+"x");
+// }
+// let angka = true
+// do{
+//     console.log("Hello World");
+// }while(angka);
+// function plugin(){
+//     console.log("function biasa")
+// }
+// let plugin = function(){
+//     console.log("fungsi expresi");
+// }
+// let plugin = () =>{
+//     console.log("function arrow");
+// }
+let adalah
+var x
+var b
+function angka(b){
+    if(b % 2 === 0){
+        return 'Genap';
+    }else{
+        return 'Ganjil';
+    }
+}
+for (x=1;x<=10;x++){
+    console.log(x+" adalah "+angka(x));
+}
+//     if(x % 2 === 0){
+//    console.log("Genap");
+// }else{
+//    console.log("Ganjil");
+// }
